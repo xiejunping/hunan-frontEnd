0edc839df219dc75830d70f7b6b43d22aadead19
Cobbler <417663733@qq.com>
2018-05-24 17:00:10 +0800

    this is a first stat

---
+++ utils/README.md
@@ -0,0 +1,29 @@
+# 工具类
+
+> 大家可以在下面添加想要收集的工具，例如：
+
+```markdown
+
+* [ ] 随机排序
+
+```
+
+
+> 或是已经添加的工具，请按照以下示例增加说明文档
+
+```js
+
+/**
+ * 记录集中查找位置
+ * @param {String} id 查找值
+ * @param {String} feild 查找属性
+ * @param {Array} arr 查找源
+ * @returns {number} 位置
+ */
+function findIndex (id, feild, arr) {
+    ...
+    return index;
+}
+
+
+```
---
+++ utils/array/index.js
@@ -0,0 +1,26 @@
+define(function (require) {
+  'use strict';
+
+  let utils = {
+    /**
+     * 记录集中查找位置
+     * @param {String} id 查找值
+     * @param {String} feild 查找属性
+     * @param {Array} arr 查找源
+     * @returns {number} 位置
+     */
+    findIndex: function (id, feild, arr) {
+      let index = -1,
+        tem = [];
+      if (id && feild && arr && arr.length) {
+        arr.forEach(function (t, i) {
+          tem.push(t[feild]);
+        });
+        index = tem.indexOf(id);
+      }
+      return index;
+    }
+  };
+
+  return utils;
+});
