c0872f9f98ae182a14531c452caff1f785656c66
Cabber <xiejunping@caohua.com>
2019-06-03 14:43:37 +0800

    漏提交文件

---
+++ examples/news/router/routes.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    path: '/news',
+    name: 'news',
+    component: resolve => {
+      require(['@/pages/news/news.vue'], resolve)
+    },
+    meta: {
+      title: '新闻'
+    }
+  },
+  {
+    path: '/github',
+    name: 'github',
+    component: resolve => {
+      require(['@/pages/news/github.vue'], resolve)
+    },
+    meta: {
+      title: '开源项目'
+    }
+  },
+  {
+    path: '/share',
+    name: 'share',
+    component: resolve => {
+      require(['@/pages/news/share.vue'], resolve)
+    },
+    meta: {
+      title: '分享'
+    }
+  },
+  {
+    path: '/website',
+    name: 'website',
+    component: resolve => {
+      require(['@/pages/news/website.vue'], resolve)
+    },
+    meta: {
+      title: '网站'
+    }
+  },
+  {
+    path: '/design',
+    name: 'design',
+    component: resolve => {
+      require(['@/pages/news/design.vue'], resolve)
+    },
+    meta: {
+      title: '设计'
+    }
+  },
+  {
+    path: '/tool',
+    name: 'tool',
+    component: resolve => {
+      require(['@/pages/news/tool.vue'], resolve)
+    },
+    meta: {
+      title: '工具'
+    }
+  }
+]
