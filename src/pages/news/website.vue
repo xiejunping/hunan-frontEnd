cd30bcef60673c0a349164648b19b874d17a96c2
Cabber <xiejunping@caohua.com>
2019-06-03 18:00:44 +0800

    增加iphonex 图片

---
+++ src/pages/news/website.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'website'
+}
+</script>
+<style lang="stylus" scoped>
+@import "~assets/styles/mixin.styl"
+</style>
+
