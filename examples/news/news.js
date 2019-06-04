c3272e7f322608d6ddd65e7dbe5784431ba5df9c
Cabber <xiejunping@caohua.com>
2019-06-04 19:24:08 +0800

    手机预研项目

--- examples/news/news.js
+++ examples/news/news.js
@@ -3,12 +3,16 @@ import iView from 'iview'
 import App from './App.vue'
 import router from './router'
 import store from '@/store'
+import VueColor from 'vue-color'
+import draggable from 'vuedraggable'
 import 'babel-polyfill'
 import 'iview/dist/styles/iview.css'
 import '@/assets/styles/index.styl'
 
 Vue.config.productionTip = false
 Vue.use(iView)
+Vue.component('draggable', draggable)
+Vue.component('chrome-picker', VueColor.Chrome)
 
 new Vue({
   render: h => h(App),
